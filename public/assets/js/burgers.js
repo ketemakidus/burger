f612d4c84cde7e7dccb7b87e8423301dadc326eb
kidus bitew <ketemakidus009@gmail.com>
2019-03-07 11:22:03 -0500

    burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,24 +1,22 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
+    $("#new-burger").on("click", function(event) {
+      event.preventDefault();
+
+      var newburger = {
+           burger_name = $("#burger-name").val().trim()
       };
-  
+      
       // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newSleepState
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newburger
       }).then(
         function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
+          console.loh(newburger);
           location.reload();
         }
-      );
+    )
     });
   
     $(".create-form").on("submit", function(event) {
