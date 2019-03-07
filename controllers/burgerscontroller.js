35dbd2ea57310c5dd1b59f3206992a96f3431ca9
kidus bitew <ketemakidus009@gmail.com>
2019-03-07 11:08:37 -0500

    burger

--- controllers/burgerscontroller.js
+++ controllers/burgerscontroller.js
@@ -4,57 +4,22 @@ var router = express.Router();
 
 var burgers = require("../models/burgers.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burgers.all(function(data) {
-    var hbsObject = {
-        burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    burgers.selectAll(function(result) {
+    res.render("index", {result});
   });
 });
 
 router.post("/api/burgers", function(req, res) {
-  burgers.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
+  burgers.insertOne(req.body.burger_name, function(result) {
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burgers.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  burgers.updateOne(req.body.devoured,req.params.id, function(result){
+res.json({ id:result });
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burgers.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-// Export routes for server.js to use.
 module.exports = router;
