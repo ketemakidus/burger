97d61c0de30627074d9b1c76c7ff0b2d8b83dc1b
kidus bitew <ketemakidus009@gmail.com>
2019-03-06 22:35:10 -0500

    burger

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -25,3 +26,4 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
